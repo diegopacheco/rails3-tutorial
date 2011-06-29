161b60558be3fdc65fa947685fd24848976fb0f5
Diego Pacheco <diego.pacheco.it@gmail.com>
2011-06-29 01:52:54 -0300

    added rails 3 tutorial files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '54337ae307139c93ccefe874c31996ea85dff7ca23707bb05f38497fc828abcf856754554ce7c0831975843bcdf6921fbd75730bfccbf67ec20de32427034780'
